81d51a23a050f46c678aaa99437779a68defab46
111031075jack <111031075@live.asia.edu.tw>
2025-08-14 19:08:55 +0800

    update

--- src/main/java/websocket/ChatServer.java
+++ src/main/java/websocket/ChatServer.java
@@ -15,29 +15,36 @@ public class ChatServer {
 		String sessionId = session.getId();
 		System.out.printf("session id: %s 已連入%n", sessionId);
 		String message = String.format("%s 已進入聊天室%n", sessionId);
-		session.getAsyncRemote().sendText(message); // 將訊息回傳給 client 端
-	}
+		if(session.isOpen()) {
+			session.getAsyncRemote().sendText(message); // 將訊息回傳給 client 端
+		}
+		}
 	
 	@OnMessage
 	public void onMessage(Session session, String message) {
 		String sessionId = session.getId();
 		System.out.printf("session id: %s 說: %s%n", sessionId, message);
 		message = String.format("%s 說%s%n", sessionId, message);
-		session.getAsyncRemote().sendText(message); // 將訊息回傳給 client 端
-	}
+		if(session.isOpen()) {
+			session.getAsyncRemote().sendText(message); // 將訊息回傳給 client 端
+		}
+		}
 	
 	@OnClose
 	public void onClose(Session session) {
 		String sessionId = session.getId();
 		System.out.printf("session id: %s 已關閉%n", sessionId);
 		String message = String.format("%s 已離開聊天室%n", sessionId);
-		session.getAsyncRemote().sendText(message); // 將訊息回傳給 client 端
-	}
+		if(session.isOpen()) {
+			session.getAsyncRemote().sendText(message); // 將訊息回傳給 client 端
+		}
+		}
 	
 	@OnError
 	public void onError(Session session, Throwable throwable) {
 		String sessionId = session.getId();
 		System.out.printf("session id: %s 發生錯誤: %s%n", sessionId, throwable);
+		
 		
 	}
 	
