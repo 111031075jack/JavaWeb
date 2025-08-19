d21869a3a38e8fd1af117bb806c33a65d2002085
111031075jack <111031075@live.asia.edu.tw>
2025-08-19 19:51:17 +0800

    update

---
+++ src/main/java/servlet/AIChatServlet.java
@@ -0,0 +1,43 @@
+package servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/aichat")
+public class AIChatServlet extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("utf-8");
+		resp.setCharacterEncoding("utf-8");
+		resp.setContentType("text/html;charset=utf-8");
+		
+		String message = req.getParameter("message"); // 使用者的提問訊息
+		String ollamaResponse = "";
+		
+		// 準備一個 json 的請求
+		String payload = """
+				{ 
+    "model": "qwen2.5:0.5b", 
+    "messages": [ 
+                    { 
+                        "role": "user", 
+                        "content": "%s" 
+              } 
+				], 
+				"stream": false 
+			}	
+				""";
+		payload = String.format(payload, message);
+		
+		// 發送 post 請求到 http://localhost:11434/api/chat
+		
+		
+	}
+	
+}
